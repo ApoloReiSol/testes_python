53876a27ea3bd9d8eb56bd00c879884090d082b1
Wanderson Oliveira <06waderson@gmail.com>
2023-09-16 10:24:17 -0300

    atividadde2_python.yp

--- Atividade2_Python.py
+++ Atividade2_Python.py
@@ -1,4 +1,22 @@
 # Calculadora simples
 
-numero1 = float (input("numero?"))
-numero2 = float (input("numero?"))
+numero_um = float(imput("digite o primeiro numero: "))
+numero_dois = float(input("digite o segundo numero: "))
+
+resultado = ''
+if operacao == '+':
+    resultado = numero_um + numero_dois
+elif operacao == '-':
+    resultado = numero_um - numero_dois
+elif operacao == '*':
+    esultado =numero_um * numero_dois
+elif operacao == '/':
+    if numero_dois == 0:
+        print("Não é possivel dividir por zero")
+    else:
+        resultado = numero_um / numero_dois
+else:
+    prin("O0peração nãorenhecida")
+
+if resultado != '':
+    print(f"O resultado é{resultado}")
